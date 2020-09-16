fd5342cc5442967684cd123938f89899a71c5c62
n1290328 <bjym1290328@gn.iwasaki.ac.jp>
2020-09-16 11:14:26 +0900

    練習02_3_1

---
+++ src/jp/ac/isc/cloud/UserSelectServlet.java
@@ -0,0 +1,65 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class UserSelectServlet
+ */
+@WebServlet("/UserSelectServlet")
+public class UserSelectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		//DB接続に使用するクラス
+		Connection users = null;
+		try {
+			//MySQL用のJDBCドライバーのクラスをロードする
+			users = DBConnection.openConnection();
+			ArrayList<Member> list = new ArrayList<Member>();
+			//SQLを実行するためのクラスを用意
+			Statement state = users.createStatement();
+			//SELECTした結果を入れるクラスを用意
+			ResultSet result = state.executeQuery("SELECT * FROM user_table");
+			//次のデータを取り出し、取り出すデータがない場合はfalseを返す
+			while (result.next()) {
+				String id = result.getString("id");
+				String name = result.getString("name");
+				String picture = result.getString("picture");
+				//Memberクラスに１件ずつレコードを登録
+				list.add(new Member(id, name, picture));
+			}
+			result.close(); //SQLの結果を受け取ったバッファを閉じる
+			DBConnection.closeConnection(users, state);
+			request.setAttribute("list", list);
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/select.jsp");
+			rd.forward(request, response);
+			//クラスが存在しなかったらエラーを表示
+
+			//SQL実行時エラーが発生したら、エラーを表示
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
